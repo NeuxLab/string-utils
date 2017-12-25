78f2e6389a721c242e86658330af147389d6946f
Acen <acenqiu@gmail.com>
2017-12-25 16:39:56 +0800

    修改协议

--- lib/main.ts
+++ lib/main.ts
@@ -1,7 +1,19 @@
+import * as _ from 'lodash'
+
+interface ValueWithType {
+  type: string
+  value: any
+}
+
 module.exports = {
 
-  base64 (input): string {
-    return new Buffer(input).toString('base64')
+  base64 (input: ValueWithType): ValueWithType {
+    const result = new Buffer(input.value).toString('base64')
+    return {type: 'text', value: result}
+  },
+
+  capitalize (input: ValueWithType): ValueWithType {
+    return {type: 'text', value: _.capitalize(input.value)}
   }
 
 }
